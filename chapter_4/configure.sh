a70dff2f218f8c1ff98f5c88f1e332af693a315c
ATTY Lionel <lionel.atty@ign.fr>
2016-03-29 16:55:21 +0200

    Travail sur les CTags

--- chapter_4/configure.sh
+++ chapter_4/configure.sh
@@ -2,5 +2,6 @@
 mkdir build 
 cd build
 #cmake -G "Unix Makefiles" ..
-cmake -G "Sublime Text 2 - Unix Makefiles" ..
+#cmake -G "Sublime Text 2 - Unix Makefiles" ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G "Sublime Text 2 - Unix Makefiles" ..
 cd -
